0dde1736c04812c73247fabb37b59b28e729640c
Juan Carlos Ulloa <ext_jculloav@falabella.cl>
2023-06-22 11:29:36 -0400

    feat: removing innecesaries tag

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -53,10 +53,10 @@
             </a>
         </li>
 
-        <li class="w-full mt-4">
+        {{--<li class="w-full mt-4">
             <h6 class="pl-6 ml-2 font-bold leading-tight uppercase text-xs opacity-100">1er Parcial</h6>
         </li>
-        {{-- @endcan --}}
+        --}}{{-- @endcan --}}{{--
 
         <li class="mt-0.5 w-full">
             <a class="py-2.7 text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap px-4 transition-colors"
@@ -116,7 +116,7 @@
                     <span class="ml-1 duration-300 opacity-100 pointer-events-none ease-soft">Reportes</span>
                 </a>
             </li>
-        @endcan
+        @endcan--}}
 
 
         {{-- seccion de componentes solo puede ser visualizado por los devs xD --}}
