f52bb7ca97e77577e3a2ae439892cf976a4307f0
Juan Carlos Ulloa <ext_jculloav@falabella.cl>
2023-06-23 14:00:51 -0400

    fix: refactor socket.io

--- resources/views/VistaDiagramas/diagramador.blade.php
+++ resources/views/VistaDiagramas/diagramador.blade.php
@@ -195,8 +195,12 @@
 
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.1/dist/html2canvas.min.js">
     </script>
-
+    <script src="https://cdn.socket.io/4.6.0/socket.io.min.js" integrity="sha384-c79GN5VsunZvi+Q/WObgk2in0CbZsHnjEqvFxC5DxHn9lTfNce2WW6h2pH6u/kF+" crossorigin="anonymous"></script>
     <script>
+        $(function(){
+            let socket = io('http://appscore.ml:3000');
+        });
+        socket.on('connection');
         //Definimos el botón para escuchar su click
         const $boton = document.querySelector("#btnCapturar"), // El botón que desencadena
             // $objetivo = document.body; // A qué le tomamos la foto
