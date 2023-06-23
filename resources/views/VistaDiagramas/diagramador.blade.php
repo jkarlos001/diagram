b64c31a28b3051907da946b39022713a78a98194
Juan Carlos Ulloa <ext_jculloav@falabella.cl>
2023-06-23 13:25:02 -0400

    feat: add socketio

--- resources/views/VistaDiagramas/diagramador.blade.php
+++ resources/views/VistaDiagramas/diagramador.blade.php
@@ -195,6 +195,13 @@
 
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.1/dist/html2canvas.min.js">
     </script>
+    <script src="https://cdn.socket.io/4.6.0/socket.io.min.js" integrity="sha384-c79GN5VsunZvi+Q/WObgk2in0CbZsHnjEqvFxC5DxHn9lTfNce2WW6h2pH6u/kF+" crossorigin="anonymous"></script>
+    <script>
+        $(function(){
+            let socket = io('http://appscore.ml:3000');
+        });
+        socket.on('connection');
+    </script>
 
     <script>
         //Definimos el botón para escuchar su click
