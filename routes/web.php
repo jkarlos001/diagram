4d0dcbc535a38810e62e7a8480df84190244a2c9
Juan Carlos Ulloa <ext_jculloav@falabella.cl>
2023-06-23 15:33:08 -0400

    fix: web routing

--- routes/web.php
+++ routes/web.php
@@ -188,8 +188,8 @@ Route::post('atributoTipoStore', [ClaseController::class, 'atributoTipoStore'])-
 
 
 Route::get('postgresql{d}', [DiagramaController::class, 'postgresql'])->name('postgresql');
+Route::get('sqlserverview{d}', [DiagramaController::class, 'sqlserverview'])->name('sqlserverview');f
 Route::get('sqlserver{d}', [DiagramaController::class, 'sqlserver'])->name('sqlserver');
-Route::get('sqlserverview{d}', [DiagramaController::class, 'sqlserverview'])->name('sqlserverview');
 
 // Route::resource('sintaxis', SintaxiController::class)
 // ->Parameters(['sintaxis' => 's'])->names('sintaxis');   //ya no usar
