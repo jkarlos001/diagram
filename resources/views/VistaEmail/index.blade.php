51947a6a7245090b9294138ebe9998d05cc5d660
Juan Carlos Ulloa <ext_jculloav@falabella.cl>
2023-06-23 10:07:26 -0400

    feat: configurando email

--- resources/views/VistaEmail/index.blade.php
+++ resources/views/VistaEmail/index.blade.php
@@ -1,59 +1,30 @@
-{{-- @extends('index');
-
-@section('encabezado')
-@endsection
-
-@section('jcst') --}}
 <div class="bg-gray-100 min-h-screen flex items-center justify-center">
     <div class="bg-white max-w-xl w-full p-8 rounded-lg shadow-lg">
-        <h2 class="text-3xl font-bold mb-6">¡Te invito a colaborar en mi plataforma de diagramas!</h2>
+        <h2 class="text-3xl font-bold mb-6">Se lo ha invitado a participar en un proyecto.</h2>
 
-        <p class="text-lg mb-6">Estimado {{ $e }},</p>
+        <p class="text-lg mb-6">Hola {{ $e }},</p>
 
-        <p class="text-lg mb-6">Mi nombre es {{ $p }},Me complace invitarte a unirte a nuestra plataforma de
-            diagramas como colaborador.</p>
+        <p class="text-lg mb-6">El anfitrion {{ $p }}, lo ha invitado a formar parte como colaborador.</p>
 
-        <p class="text-lg mb-6">En el diagrama {{ $d }}, nos dedicamos a proporcionar una plataforma intuitiva
-            y
-            poderosa para crear y colaborar en diagramas de manera eficiente. Creemos que tus habilidades y experiencia
-            serían una gran adición a nuestra comunidad de colaboradores.</p>
+        <p class="text-lg mb-6">El proyecto: {{ $d }}, esta disponible para su participación..</p>
 
-        <p class="text-lg mb-6">Nuestra plataforma ofrece una amplia gama de características y herramientas, incluyendo:
-        </p>
-
-        <ul class="list-disc list-inside text-lg mb-6">
-            <li>Editor de diagramas fácil de usar y con una interfaz intuitiva.</li>
-            <li>Guarda tus cambios automaticamente.</li>
-            <li>Funcionalidades avanzadas, como la posibilidad de colaborar en tiempo real con otros usuarios</li>
-            <li>Permite Exportación de diagramas en varios formatos.</li>
-        </ul>
-
-        <p class="text-lg mb-6">Si estás interesado en unirte a nosotros, simplemente sigue los siguientes pasos:</p>
+        <p class="text-lg mb-6">Si estás interesado en unirte sigue los siguientes pasos:</p>
 
         <ol class="list-decimal list-inside text-lg mb-6">
             <li>Haz clic en el siguiente enlace para registrarte en nuestra plataforma:
-                <a href="https://appscore.ml/">DIAGRAMADOR UML</a>
+                <a href="https://appscore.ml/">Proyecto</a>
             </li>
-            <li>Completa el proceso de registro y crea tu perfil.</li>
-            <li>Ve a la seccion de diagramas y podras ver la lista de diagramas en colaboracion.</li>
+            <li>Rellena los campos.</li>
+            <li>Ve a la seccion de diagramas y podras ver la lista de proyectos en los cual estás asociado.</li>
         </ol>
 
-        <p class="text-lg mb-6">Si tienes alguna pregunta o necesitas asistencia durante el proceso de registro, no
-            dudes en contactarme directamente a través de este correo electrónico.</p>
+        <p class="text-lg mb-6">Se recomienda utilizar esta misma dirección de correo electronico para participar en el proyecto.</p>
 
-        <p class="text-lg mb-6">Esperamos contar contigo como colaborador.</p>
-
-        <p class="text-lg">¡Gracias por tu tiempo y consideración!</p>
+        <p class="text-lg">¡Gracias por colaborar!</p>
 
         <div class="mt-8 text-gray-600">
-            <p class="text-lg mb-2">Saludos cordiales,</p>
-            <p class="text-lg mb-0">{{ $p }}</p>
-            <p class="text-lg mb-0">JCST diagramador COMPANY</p>
-            <p class="text-lg">76034449</p>
+            <p class="text-lg mb-2">Saludos.</p>
         </div>
     </div>
 </div>
-{{-- @endsection
 
-@section('scripts')
-@endsection --}}
