4b9d2b1814215f78d4ace9f999f1405532240f7a
Juan Carlos Ulloa <ext_jculloav@falabella.cl>
2023-06-23 14:05:09 -0400

    fix: refactor socket.io

--- resources/views/VistaDiagramas/diagramador.blade.php
+++ resources/views/VistaDiagramas/diagramador.blade.php
@@ -193,9 +193,6 @@
 
     <script src="./js/diagrama.js" async></script>
 
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.1/dist/html2canvas.min.js">
-    </script>
-    <script src="https://cdn.socket.io/4.6.0/socket.io.min.js" integrity="sha384-c79GN5VsunZvi+Q/WObgk2in0CbZsHnjEqvFxC5DxHn9lTfNce2WW6h2pH6u/kF+" crossorigin="anonymous"></script>
     <script>
         $(function(){
             let socket = io('http://appscore.ml:3000');
