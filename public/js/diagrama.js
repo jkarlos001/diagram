8c08a655933110a596dc0ecb71e782fb61b1b78a
Juan Carlos Ulloa <ext_jculloav@falabella.cl>
2023-06-25 16:01:45 -0400

    feat: testing socket

--- public/js/diagrama.js
+++ public/js/diagrama.js
@@ -1,6 +1,6 @@
 function init() {
     const $ = go.GraphObject.make;
-    const socket = io('http://appscore.ml:3000');
+    const socket = io('https://appscore.ml:3000');
     socket.on('connection');
     myDiagram =
         $(go.Diagram, "myDiagramDiv",
